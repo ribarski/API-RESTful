a527839fe737c75a19007706d9014145b5f90192
Alexandre Destro <aalexandredestro@gmail.com>
2024-11-19 21:56:46 -0300

    a

--- SaudeBemEstar/src/main/java/com/example/SaudeBemEstar/atendimento/controller/AtendimentoController.java
+++ SaudeBemEstar/src/main/java/com/example/SaudeBemEstar/atendimento/controller/AtendimentoController.java
@@ -1,15 +1,26 @@
 package com.example.SaudeBemEstar.atendimento.controller;
 
-import com.example.SaudeBemEstar.atendimento.dto.AtendimentoDTO;
-import com.example.SaudeBemEstar.atendimento.service.AtendimentoService;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.SaudeBemEstar.atendimento.dto.AtendimentoDTO;
+import com.example.SaudeBemEstar.atendimento.service.AtendimentoService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 @RestController
 @RequestMapping("/atendimentos")
@@ -38,6 +49,7 @@ public class AtendimentoController {
 
     // Criar atendimento
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<AtendimentoDTO> criarAtendimento(@RequestBody @Valid AtendimentoDTO atendimentoDTO) {
         return new ResponseEntity<>(atendimentoService.criarAtendimento(atendimentoDTO), HttpStatus.CREATED);
     }
--- SaudeBemEstar/src/main/java/com/example/SaudeBemEstar/recomendacao/mapper/RecomendacaoMapper.java
+++ SaudeBemEstar/src/main/java/com/example/SaudeBemEstar/recomendacao/mapper/RecomendacaoMapper.java
@@ -1,13 +1,16 @@
 package com.example.SaudeBemEstar.recomendacao.mapper;
 
-import com.example.SaudeBemEstar.recomendacao.dto.RecomendacaoDTO;
-import com.example.SaudeBemEstar.recomendacao.model.Recomendacao;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import com.example.SaudeBemEstar.recomendacao.dto.RecomendacaoDTO;
+import com.example.SaudeBemEstar.recomendacao.model.Recomendacao;
+
 @Mapper(componentModel = "spring")
 public interface RecomendacaoMapper {
-
+    @Mapping(target="atendimentoId", source="id")
     RecomendacaoDTO toRecomendacaoDTO(Recomendacao recomendacao);
+    
+    @Mapping(target="atendimento.id", source="atendimentoId")
     Recomendacao toRecomendacao(RecomendacaoDTO recomendacaoDTO);
 }
